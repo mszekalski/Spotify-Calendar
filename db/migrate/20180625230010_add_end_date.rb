99fd680f6fe23c6d252d781ba2afb13596f91e30
Matthew Szekalski <mattszekalski@icloud.com>
2018-06-25 19:08:08 -0400

    fixed table

---
+++ db/migrate/20180625230010_add_end_date.rb
@@ -0,0 +1,6 @@
+class AddEndDate < ActiveRecord::Migration[5.2]
+  def change
+    add_column :events, :end_date, :date
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_22_210428) do
+ActiveRecord::Schema.define(version: 2018_06_25_230010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
     t.string "description"
-    t.date "date"
+    t.date "start_date"
     t.time "start"
     t.time "end"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "end_date"
   end
 
 end
