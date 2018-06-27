590dd30e9292f5061ec051af1280ad4514bc500e
Matthew Szekalski <mattszekalski@icloud.com>
2018-06-27 16:36:43 -0400

    added Day container

--- frontend/components/calendar/calendar_area.jsx
+++ frontend/components/calendar/calendar_area.jsx
@@ -3,15 +3,37 @@ import React, { Component } from "react";
 import { withRouter } from "react-router";
 import { Route, Link } from "react-router-dom";
 import { connect } from "react-redux";
+import DayContainer from "./day_container";
+
+const DAYS = {
+  January: 31,
+  Feburary: 28,
+  March: 31,
+  April: 30,
+  May: 31,
+  June: 30,
+  July: 31,
+  August: 31,
+  September: 30,
+  October: 31,
+  November: 30,
+  December: 31
+};
 
 class CalendarArea extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: this.props.currentDate
+      date: this.props.currentDate,
+      month: null
     };
+    this.days = DAYS;
     this.nextMonth = this.nextMonth.bind(this);
     this.prevMonth = this.prevMonth.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ month: this.state.date.month() });
   }
 
   renderDaysHeader() {
@@ -31,17 +53,32 @@ class CalendarArea extends React.Component {
   nextMonth() {
     let date = this.state.date;
     date.add(1, "M");
-    this.setState({ date: date });
+    this.setState({ date: date, month: date.month() });
   }
 
   prevMonth() {
     let date = this.state.date;
     date.add(-1, "M");
-    this.setState({ date: date });
+    this.setState({ date, month: date.month() });
   }
 
   renderMonth() {
     return <span>{this.state.date.format("MMMM")}</span>;
+  }
+
+  renderDays() {
+    let daysOfTheWeek = ["SUN", "MON", "TUES", "WED", "THURS", "FRI", "SAT"];
+    let numberOfDays = this.days[Object.keys(this.days)[this.state.month]];
+    let firstDate = this.state.date.date(1);
+    let firstDay = daysOfTheWeek[firstDate.day()];
+    const days = [];
+    for (let i = 0; i < firstDay; i++) {
+      days.push(<Day />);
+    }
+    for (let i = 1; i < numberOfDays; i++) {
+      days.push(<Day />);
+    }
+    return days;
   }
 
   render() {
@@ -57,7 +94,7 @@ class CalendarArea extends React.Component {
         </button>
 
         {this.renderDaysHeader()}
-        {this.renderWeeks()}
+        <div>{this.renderDays()}</div>
       </div>
     );
   }
---
+++ frontend/components/day/day.jsx
@@ -0,0 +1,15 @@
+import moment from "moment";
+import React, { Component } from "react";
+import { withRouter } from "react-router";
+import { Route, Link } from "react-router-dom";
+import { connect } from "react-redux";
+
+class Day extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {}
+}
+
+export default Day;
